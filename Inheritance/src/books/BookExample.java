9fdb6d06b61c476e1248dac25e10b9d11d16dbca
eunjin <dididiwlgus@nate.com>
2021-04-26 16:12:49 +0900

    [Inheritance] books samples

---
+++ Inheritance/src/books/BookExample.java
@@ -0,0 +1,18 @@
+package books;
+
+public class BookExample {
+
+	public static void main(String[] args) {
+		Book book = new Book("Áø´Þ·¡ ²É", "½Ã", "±è¼Ò¿ù", 20.5);
+		
+		System.out.print("[½ÃÁý]");
+		System.out.println("name: " + book.name);
+		System.out.println("catetory: " + book.category);
+		System.out.println("author: " + book.author);
+		System.out.println("price: " + book.price);
+		
+		System.out.println("-------------------------------");
+		System.out.println(book);
+	}
+
+}
