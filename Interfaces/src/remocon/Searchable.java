6e8be6f74101a2857fd7a34c5567b40c12b56ee0
eunjin <dididiwlgus@nate.com>
2021-05-03 11:35:16 +0900

    [Interface] remocon: 다중 인터페이스 구현 예제

--- Interfaces/src/remocon/Searchable.java
+++ Interfaces/src/remocon/Searchable.java
@@ -2,4 +2,6 @@ package remocon;
 
 public interface Searchable {
 	void search(String url);
+	default void find(String text) {}
+	
 }
---
+++ Interfaces/src/remocon/Searcher.java
@@ -0,0 +1,16 @@
+package remocon;
+
+public class Searcher implements Searchable{
+	
+	
+	@Override
+	public void search(String url) {
+		System.out.printf("SearcherFinder:search(%s)\n", url);
+	
+	}
+	public static void main(String args[]) {
+	Searcher search = new Searcher();
+	search.search("https://naver.com");
+	}
+}
+
