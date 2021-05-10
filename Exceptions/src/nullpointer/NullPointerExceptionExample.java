e015bfb3e782424c9f679a0d305f64d05520e6c1
eunjin <dididiwlgus@nate.com>
2021-05-10 17:51:14 +0900

    [Exceptions] 예외처리 예제

---
+++ Exceptions/src/nullpointer/NullPointerExceptionExample.java
@@ -0,0 +1,11 @@
+package nullpointer;
+
+public class NullPointerExceptionExample {
+
+	public static void main(String[] args) {
+		String data = null;
+		System.out.println(data.toString()); // java.lang.NullPointerException
+
+	}
+
+}
